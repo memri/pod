232f052185cd17399625a8653fe3e3e0c007cc72
Vasili Novikov <n1dr+cm3513git@ya.ru>
2020-04-10 14:10:16 +0200

    write openapi to file explicitly

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod api_model;
 use api::*;
 use rweb::rt::IndexMap;
 use rweb::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 #[tokio::main]
 async fn main() {
@@ -22,6 +24,11 @@ async fn main() {
     });
     let (spec, _filter) = spec.build(|| version().or(create()));
 
-    // only print the OpenAPI definition for now.
-    println!("{}", serde_json::to_string_pretty(&spec).unwrap());
+    // only write the OpenAPI definition fo a file, for now.
+    let openapi_spec_string = serde_json::to_string_pretty(&spec)
+        .unwrap_or_else(|err| panic!("Failed to serialize openapi specification to json, {}", err));
+    let mut file = File::create("target/openapi.json")
+        .unwrap_or_else(|err| panic!("Failed to open openapi.json for writing, {}", err));
+    file.write_all(openapi_spec_string.as_bytes())
+        .unwrap_or_else(|err| panic!("Failed to write openapi.json, {}", err));
 }
