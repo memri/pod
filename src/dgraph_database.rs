9cc1c78d66b4976fae2aa09369fedd50aa25ae91
Vasili Novikov <n1dr+cm3513git@ya.ru>
2020-05-22 20:04:14 +0200

    add basic logging on importand Dgraph operations

--- src/dgraph_database.rs
+++ src/dgraph_database.rs
@@ -1,6 +1,7 @@
 use crate::data_model;
 use config::Config;
 use dgraph::*;
+use log::info;
 
 /// Create dgraph gRPC connection.
 pub fn create_dgraph(settings: &Config) -> Dgraph {
@@ -11,6 +12,7 @@ pub fn create_dgraph(settings: &Config) -> Dgraph {
 
 /// Drop all schema and data.
 pub fn drop_schema_and_all_data_irreversibly(dgraph: &Dgraph) {
+    info!("Dropping Dgraph schema and all associated data.");
     let op_drop = dgraph::Operation {
         drop_all: true,
         ..Default::default()
@@ -23,6 +25,7 @@ pub fn drop_schema_and_all_data_irreversibly(dgraph: &Dgraph) {
 /// Create schema for all properties and types.
 /// Add schema to dgraph.
 pub fn add_schema(dgraph: &Dgraph) {
+    info!("Adding full Dgraph schema.");
     let edge_props = data_model::dgraph_edge_properties();
     let node_props = data_model::dgraph_node_string_properties();
     let types = data_model::create_types();
