4d7211dbb62f7a4cd05ab89374c9866a21ef0767
Bijun Li <bijun.li@polis.global>
2020-06-09 12:18:11 +0200

    Update "start-pod.sh" with volume

--- tools/start-pod.sh
+++ tools/start-pod.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -p 3030:3030 -e ADD_SCHEMA_ON_START=true -e DGRAPH_HOST=pod_dgraph_1:9080 --network my-net --name pod_pod_1 pod:latest
+docker run --rm -it -p 3030:3030 -e ADD_SCHEMA_ON_START=true -e DGRAPH_HOST=pod_dgraph_1:9080 --network my-net --name pod_pod_1 -v download-volume:/data pod:latest
