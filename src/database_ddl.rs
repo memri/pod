3b895b08c5a8ed6a1808a5340d5fd4dd1802e2f7
Bijun Li <bijun.li@polis.global>
2020-06-23 17:01:30 +0200

    Update for merge

--- src/database_ddl.rs
+++ src/database_ddl.rs
@@ -1,40 +1,35 @@
 use std::string::String;
 
 pub fn create_mandatory_tables() {
-    let items = String::from(
-        "CREATE TABLE IF NOT EXISTS items (
+    let items = "CREATE TABLE IF NOT EXISTS items (
         id INTEGER NOT NULL PRIMARY KEY,
         type TEXT NOT NULL,
         created_at REAL NOT NULL,
         modified_at REAL NOT NULL,
         read_at REAL,
         version INTEGER NOT NULL
-        )",
-    );
+        )";
 
-    let items_indexes = String::from(
-        "CREATE INDEX IF NOT EXISTS idx_items_type ON items (type);
+    let items_indexes = "CREATE INDEX IF NOT EXISTS idx_items_type ON items (type);
         CREATE INDEX IF NOT EXISTS idx_items_created_at ON items (created_at);
         CREATE INDEX IF NOT EXISTS idx_items_modified_at ON items (modified_at);
-        CREATE INDEX IF NOT EXISTS idx_items_read_at ON items (read_at) WHERE read_at IS NOT NULL;",
-    );
+        CREATE INDEX IF NOT EXISTS idx_items_read_at ON items (read_at) WHERE read_at IS NOT NULL;";
 
-    let relations = String::from(
-        "CREATE TABLE IF NOT EXISTS relations (
+    let relations = "CREATE TABLE IF NOT EXISTS relations (
         source INTEGER NOT NULL,
-        FOREIGN KEY (source) REFERENCES items (id);
         target INTEGER NOT NULL,
-        FOREIGN KEY (target) REFERENCES items (id);
         type TEXT NOT NULL,
         created_at REAL NOT NULL,
         modified_at REAL NOT NULL,
         read_at REAL,
+        FOREIGN KEY (source) REFERENCES items (id),
+        FOREIGN KEY (target) REFERENCES items (id),
         UNIQUE(source, target, type)
-        )",
-    );
+        )";
 
-    let relations_indexes = String::from(
+    let relations_indexes =
         "CREATE INDEX IF NOT EXISTS idx_relations_direct ON relations (source, type);
-        CREATE INDEX IF NOT EXISTS idx_relations_reverse ON relations (target, type);",
-    );
+        CREATE INDEX IF NOT EXISTS idx_relations_reverse ON relations (target, type);
+        CREATE INDEX IF NOT EXISTS idx_relations_modified_at ON relations (modified_at);
+        CREATE INDEX IF NOT EXISTS idx_relations_read_at ON relations (read_at);";
 }
