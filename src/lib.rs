bda1ec1055020b94f09f0c763bc7b1450c30a44d
Vasili Novikov <n1dr+cm3513git@ya.ru>
2020-06-23 12:55:24 +0200

    remove dgraph from codebase

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 // Fake simple library interface to allow integration tests to work
 
 pub mod data_model;
-pub mod dgraph_database;
-pub mod importers;
 pub mod internal_api;
-pub mod sync_state;
 pub mod warp_api;
--- src/warp_api.rs
+++ src/warp_api.rs
@@ -1,18 +1,15 @@
-use crate::importers::note_importer;
 use crate::internal_api;
 use bytes::Bytes;
-use dgraph::Dgraph;
 use log::debug;
 use log::info;
-use std::sync::Arc;
+use log::warn;
 use warp::http::StatusCode;
 use warp::Filter;
 use warp::Reply;
 
 /// Start web framework with specified APIs.
-pub async fn run_server(server_name: String, dgraph: Dgraph) {
-    info!("Starting {} HTTP server", server_name);
-    let dgraph = Arc::new(dgraph);
+pub async fn run_server() {
+    info!("Starting {} HTTP server", env!("CARGO_PKG_NAME").to_uppercase());
     // Get version of cargo project POD.
     let version = warp::path("version")
         .and(warp::path::end())
@@ -25,13 +22,12 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     //     mid: memriID of requested node, String.
     // Return an array of nodes with requested memriID.
     // Return StatusCode::NOT_FOUND if node does not exist.
-    let dgraph_clone = dgraph.clone();
     let get_item = api_version_1
         .and(warp::path!("items" / String))
         .and(warp::path::end())
         .and(warp::get())
         .map(move |mid: String| {
-            let string = internal_api::get_item(&dgraph_clone, mid);
+            let string = internal_api::get_item(mid);
             let boxed: Box<dyn Reply> = if let Some(string) = string {
                 let json: serde_json::Value = serde_json::from_str(&string).unwrap();
                 debug!("Response: {}", &json);
@@ -44,13 +40,12 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     // GET API for all nodes.
     // Return an array of all nodes.
     // Return StatusCode::NOT_FOUND if nodes not exist.
-    let dgraph_clone = dgraph.clone();
     let get_all_items = api_version_1
         .and(warp::path!("all"))
         .and(warp::path::end())
         .and(warp::get())
         .map(move || {
-            let string = internal_api::get_all_items(&dgraph_clone);
+            let string = internal_api::get_all_items();
             let boxed: Box<dyn Reply> = if let Some(string) = string {
                 let json: serde_json::Value = serde_json::from_str(&string).unwrap();
                 debug!("Response: {}", &json);
@@ -64,20 +59,19 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     // Input: json of created node within the body.
     // Return uid of created node if node is unique.
     // Return StatusCode::CONFLICT if node already exists.
-    let dgraph_clone = dgraph.clone();
     let create_item = api_version_1
         .and(warp::path("items"))
         .and(warp::path::end())
         .and(warp::post())
         .and(warp::body::json())
         .map(move |body: serde_json::Value| {
-            let uid = internal_api::create_item(&dgraph_clone, body);
+            let uid = internal_api::create_item(body);
             let boxed: Box<dyn Reply> = if let Some(uid) = uid {
                 let json = serde_json::json!(uid);
                 debug!("Response: {}", &json);
                 Box::new(warp::reply::json(&json))
             } else {
-                Box::new(warp::reply::with_status(format!("Item contains an uid that already exists in the database, use update() instead."), StatusCode::CONFLICT))
+                Box::new(warp::reply::with_status("Item contains an uid that already exists in the database, use update() instead.", StatusCode::CONFLICT))
             };
             boxed
         });
@@ -87,14 +81,13 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     // Return without body:
     //     StatusCode::OK if node has been updated successfully.
     //     StatusCode::NOT_FOUND if node is not found in the database.
-    let dgraph_clone = dgraph.clone();
     let update_item = api_version_1
         .and(warp::path!("items" / String))
         .and(warp::path::end())
         .and(warp::put())
         .and(warp::body::json())
         .map(move |mid: String, body: serde_json::Value| {
-            let result = internal_api::update_item(&dgraph_clone, mid, body);
+            let result = internal_api::update_item(mid, body);
             if result {
                 StatusCode::OK
             } else {
@@ -107,13 +100,12 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     // Return without body:
     //     StatusCode::OK if node has been deleted successfully.
     //     StatusCode::NOT_FOUND if node was not found in the database.
-    let dgraph_clone = dgraph.clone();
     let delete_item = api_version_1
         .and(warp::path!("items" / String))
         .and(warp::path::end())
         .and(warp::delete())
         .map(move |mid: String| {
-            let result = internal_api::delete_item(&dgraph_clone, mid);
+            let result = internal_api::delete_item(mid);
             if result {
                 StatusCode::OK
             } else {
@@ -124,14 +116,13 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
     // Input: json of query within the body.
     // Return an array of nodes.
     // Return StatusCode::NOT_FOUND if nodes not exist.
-    let dgraph_clone = dgraph.clone();
     let query = api_version_1
         .and(warp::path("all"))
         .and(warp::path::end())
         .and(warp::post())
         .and(warp::body::bytes())
         .map(move |body: Bytes| {
-            let string = internal_api::query(&dgraph_clone, body);
+            let string = internal_api::query(body);
             let boxed: Box<dyn Reply> = if let Some(string) = string {
                 let json: serde_json::Value = serde_json::from_str(&string).unwrap();
                 debug!("Response: {}", &json);
@@ -142,7 +133,6 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
             boxed
         });
     // IMPORT API to start importing notes.
-    let dgraph_clone = dgraph.clone();
     let import_notes = api_version_1
         .and(warp::path("import"))
         .and(warp::path::param())
@@ -153,15 +143,14 @@ pub async fn run_server(server_name: String, dgraph: Dgraph) {
             info!("trying to import {} from {}", import_type, import_service);
             match (import_service.as_str(), import_type.as_str()) {
                 ("Evernote", "notes") => {
-                    note_importer::import_notes(&dgraph_clone, "data/Evernote".to_string())
+                    unimplemented!()
                 }
                 ("iCloud", "notes") => {
-                    note_importer::import_notes(&dgraph_clone, "data/iCloud".to_string())
                 }
-                (_, "notes") => info!("UNKNOWN SERVICE : {}", import_service),
-                (_, _) => info!("UNKNOWN TYPE : {}", import_type),
+                (_, "notes") => warn!("UNKNOWN SERVICE : {}", import_service),
+                (_, _) => warn!("UNKNOWN TYPE : {}", import_type),
             }
-            format!("trying to import {} from {}", import_type, import_service)
+            ""
         });
 
     // Specify APIs.
