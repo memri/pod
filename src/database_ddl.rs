67d32be6a72f61a74cbafc41b0c950600196b05a
Bijun Li <bijun.li@polis.global>
2020-06-23 15:40:44 +0200

    Add indexes

--- src/database_ddl.rs
+++ src/database_ddl.rs
@@ -7,20 +7,48 @@ pub fn create_mandatory_tables() {
         type text NOT NULL,
         created_at text NOT NULL,
         modified_at text NOT NULL,
-        read_by_user_at text NOT NULL,
+        read_at text,
         version integer NOT NULL,
-        memri_id text NOT NULL UNIQUE
+        memri_id text
         )",
     );
 
-    let edges = String::from(
+    let items_indexes = String::from(
+        "CREATE UNIQUE INDEX idx_items_id
+        ON items (id);
+        CREATE INDEX idx_items_memri_type
+        ON items (type);
+        CREATE INDEX idx_items_memri_created_at
+        ON items (created_at);
+        CREATE INDEX idx_items_memri_modified_at
+        ON items (modified_at);
+        CREATE INDEX idx_items_memri_read_at
+        ON items (read_at);
+        CREATE UNIQUE INDEX idx_items_memri_id
+        ON items (memri_id);",
+    );
+
+    let relations = String::from(
         "CREATE TABLE IF NOT EXISTS edges (
         source integer NOT NULL,
         target integer NOT NULL,
         type text NOT NULL,
-        created_at text,
-        modified_at text,
+        created_at text NOT NULL,
+        modified_at text NOT NULL,
         read_at text
         )",
+    );
+
+    let relations_indexes = String::from(
+        "CREATE UNIQUE INDEX idx_relations_source
+        ON relations (source);
+        CREATE UNIQUE INDEX idx_relations_target
+        ON relations (target);
+        CREATE UNIQUE INDEX idx_relations_type
+        ON relations (type);
+        CREATE INDEX idx_relations_direct
+        ON relations (source, type);
+        CREATE INDEX idx_relations_reverse
+        ON relations (target, type);",
     );
 }
