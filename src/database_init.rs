2ed152fb3a6009d4bd9609141ca79bf1c60ae3a3
Ruben Daniels <ruben.daniels@gmail.com>
2020-06-26 14:30:54 +0200

    Change column names to names needed by the frontend

--- src/database_init.rs
+++ src/database_init.rs
@@ -167,46 +167,46 @@ where
 
 const MANDATORY_ITEMS_FIELDS: &[&str] = &[
     "uid",
-    "_type",
-    "created_at",
-    "modified_at",
-    "read_at",
-    "deleted_at",
+    "type",
+    "dateCreated",
+    "dateModified",
+    "dateAccessed",
+    "deleted",
     "version",
 ];
 
 const ITEMS_TABLE_DDL: &str = "CREATE TABLE IF NOT EXISTS items (
     uid INTEGER NOT NULL PRIMARY KEY,
-    _type TEXT NOT NULL,
-    created_at REAL NOT NULL,
-    modified_at REAL NOT NULL,
-    read_at REAL,
+    type TEXT NOT NULL,
+    dateCreated REAL NOT NULL,
+    dateModified REAL NOT NULL,
+    dateAccessed REAL,
     deleted_at REAL,
     version INTEGER NOT NULL
 );";
 
 const ITEMS_INDEX_DDL: &str = "
-    CREATE INDEX IF NOT EXISTS idx_items_created_at ON items(created_at);
-    CREATE INDEX IF NOT EXISTS idx_items_modified_at ON items(modified_at);
-    CREATE INDEX IF NOT EXISTS idx_items_read_at ON items(read_at) WHERE read_at IS NOT NULL;
-    CREATE INDEX IF NOT EXISTS idx_items_type_modified_at ON items(_type, modified_at);
+    CREATE INDEX IF NOT EXISTS idx_items_created_at ON items(dateCreated);
+    CREATE INDEX IF NOT EXISTS idx_items_modified_at ON items(dateModified);
+    CREATE INDEX IF NOT EXISTS idx_items_read_at ON items(dateAccessed) WHERE dateAccessed IS NOT NULL;
+    CREATE INDEX IF NOT EXISTS idx_items_type_modified_at ON items(type, dateModified);
 ";
 
 const RELATIONS_TALBLE_DDL: &str = "CREATE TABLE IF NOT EXISTS relations (
     source INTEGER NOT NULL,
     target INTEGER NOT NULL,
-    _type TEXT NOT NULL,
-    created_at REAL NOT NULL,
-    modified_at REAL NOT NULL,
-    read_at REAL,
+    type TEXT NOT NULL,
+    dateCreated REAL NOT NULL,
+    dateModified REAL NOT NULL,
+    dateAccessed REAL,
     FOREIGN KEY (source) REFERENCES items(uid),
     FOREIGN KEY (target) REFERENCES items(uid),
-    UNIQUE(source, target, _type)
+    UNIQUE(source, target, type)
 );";
 
 const RELATION_INDEXES_DDL: &str = "
-    CREATE INDEX IF NOT EXISTS idx_relations_source_type ON relations(source, _type);
-    CREATE INDEX IF NOT EXISTS idx_relations_target_type ON relations(target, _type);
-    CREATE INDEX IF NOT EXISTS idx_relations_modified_at ON relations(modified_at);
-    CREATE INDEX IF NOT EXISTS idx_relations_read_at ON relations(read_at) WHERE read_at IS NOT NULL;
+    CREATE INDEX IF NOT EXISTS idx_relations_source_type ON relations(source, type);
+    CREATE INDEX IF NOT EXISTS idx_relations_target_type ON relations(target, type);
+    CREATE INDEX IF NOT EXISTS idx_relations_modified_at ON relations(dateModified);
+    CREATE INDEX IF NOT EXISTS idx_relations_read_at ON relations(dateAccessed) WHERE dateAccessed IS NOT NULL;
 ";
