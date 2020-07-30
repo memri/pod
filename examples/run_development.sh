792bbd4bbd2edc3e603a64168fa6f00dedea1380
Vasili Novikov <vasya.novikov+cm3513git@gmail.com>
2020-07-30 12:14:05 +0200

    Fix example development scripts

--- examples/run_development.sh
+++ examples/run_development.sh
@@ -2,7 +2,7 @@
 # This is an example of how you might run Pod during development.
 # Do not ever attempt to ever use this script in production!
 
-if !test -e Cargo.toml; then
+if ! test -e Cargo.toml; then
   echo "Please run this script from Pod-s main directory."
   exit 1
 fi
