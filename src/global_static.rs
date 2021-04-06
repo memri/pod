fa9fb049747e78dd836c490d1d98135e762b9ac3
Vasili Novikov <vasya.novikov+cm3513git@gmail.com>
2021-04-06 19:27:03 +0200

    Add comment to the global/static cipher

--- src/global_static.rs
+++ src/global_static.rs
@@ -9,6 +9,11 @@ use chacha20poly1305::XChaCha20Poly1305;
 use lazy_static::lazy_static;
 
 lazy_static! {
+    /// Global/static Cipher used for PluginAuth.
+    /// All plugins will be started with database_key (and potentially other keys) encrypted
+    /// via this Cipher. This cipher is re-initialized randomly upon restart
+    /// If you want to read more on the usage of this key in Pod,
+    /// check documentation in HTTP_API.md and Plugins.md.
     pub static ref CIPHER: XChaCha20Poly1305 =
         crate::plugin_auth_crypto::generate_xchacha20poly1305_cipher();
 }
