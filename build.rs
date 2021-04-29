e3094609311f5acb46cdc7501e1a7385361da405
Vasili Novikov <vasya.novikov+cm3513git@gmail.com>
2021-04-29 13:37:32 +0200

    Fix formatting

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 use std::process::Command;
 fn main() {
-    let git_describe_command = Command::new("git")
-        .args(&["describe"])
-        .output();
+    let git_describe_command = Command::new("git").args(&["describe"]).output();
     let git_hash = if let Ok(git_describe) = git_describe_command {
         String::from_utf8_lossy(&git_describe.stdout).to_string()
     } else {
