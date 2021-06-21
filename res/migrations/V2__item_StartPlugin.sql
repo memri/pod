ab4c3ffa6593adbf99c82a2af3e40565ce9780f4
Vasili Novikov <vasya.novikov+cm3513git@gmail.com>
2021-06-21 08:57:42 +0200

    Update PluginRun / StartPlugin item
    * Rename StartPlugin -> PluginRun
    * Rename PluginRun.container -> PluginRun.containerImage
      This terminology is technically better as docker runs
      container _images_ in _containers_.
    * Reset database (via name change) due to breaking
      change in migration files content.
      (This could be avoided, but no need for now.)
    * Update v3 -> v4 because a rename is a breaking change

--- res/migrations/V2__item_StartPlugin.sql
+++ res/migrations/V2__item_StartPlugin.sql
@@ -1,29 +1,29 @@
--- Items of type "StartPlugin" have property "container" (text)
+-- Items of type "PluginRun" have property "containerImage" (text)
 INSERT INTO items(id, type, dateCreated, dateModified, dateServerModified, deleted) VALUES(
     "8c914705a392432381561db94da76ad9",
     "ItemPropertySchema", 0, 0, 0, 0
 );
 INSERT INTO strings(item, name, value) VALUES(
     (SELECT rowid FROM items WHERE id = "8c914705a392432381561db94da76ad9"),
-    "itemType", "StartPlugin"
+    "itemType", "PluginRun"
 );
 INSERT INTO strings(item, name, value) VALUES(
     (SELECT rowid FROM items WHERE id = "8c914705a392432381561db94da76ad9"),
-    "propertyName", "container"
+    "propertyName", "containerImage"
 );
 INSERT INTO strings(item, name, value) VALUES(
     (SELECT rowid FROM items WHERE id = "8c914705a392432381561db94da76ad9"),
     "valueType", "text"
 );
 
--- Items of type "StartPlugin" have property "targetItemId" (text)
+-- Items of type "PluginRun" have property "targetItemId" (text)
 INSERT INTO items(id, type, dateCreated, dateModified, dateServerModified, deleted) VALUES(
     "5f43d3c0120c4f0cac3c98b8129c9cb7",
     "ItemPropertySchema", 0, 0, 0, 0
 );
 INSERT INTO strings(item, name, value) VALUES(
     (SELECT rowid FROM items WHERE id = "5f43d3c0120c4f0cac3c98b8129c9cb7"),
-    "itemType", "StartPlugin"
+    "itemType", "PluginRun"
 );
 INSERT INTO strings(item, name, value) VALUES(
     (SELECT rowid FROM items WHERE id = "5f43d3c0120c4f0cac3c98b8129c9cb7"),
